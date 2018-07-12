1c59d24423d71dd0e18500843c25ba720b581f9b
casperOne <casperOne@caspershouse.com>
2018-07-11 22:16:57 -0400

    Fixed constructor on GoogleApiMessageHandler.

--- src/Ofl.Google/GoogleApiMessageHandler.cs
+++ src/Ofl.Google/GoogleApiMessageHandler.cs
@@ -10,7 +10,7 @@ namespace Ofl.Google
     {
         #region Constructor.
 
-        internal GoogleApiMessageHandler(
+        public GoogleApiMessageHandler(
             IApiKeyProvider apiKeyProvider)
         {
             // Validate parameters.
